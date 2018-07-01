3969d80bbb8294a817da0f6e1bb2e12635063f4e
Victoria Meng <nights1ststar@protonmail.com>
2018-07-01 19:33:44 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,12 +1,12 @@
 class SchoolClassesController < ApplicationController
   def create
-    @student_class = SchoolClass.new(post_params(:title, :room_number))
+    @student_class = SchoolClass.new(school_classes_params(:title, :room_number))
     @student_class.save
   end
 
   private
 
-  def post_params(*args)
-    params.require(:post).permit(*args)
+  def school_classes_params(*args)
+    params.require(:school_class).permit(*args)
   end
 end
