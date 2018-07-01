970047d3180f09d846632ce37d0afab0f2992ac0
Victoria Meng <nights1ststar@protonmail.com>
2018-07-01 19:40:26 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,7 +1,7 @@
 class SchoolClassesController < ApplicationController
   def new
   end
-  
+
   def create
     @student_class = SchoolClass.new(school_classes_params(:title, :room_number))
     @student_class.save
